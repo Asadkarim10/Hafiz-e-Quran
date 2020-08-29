99da92e384c32620b0dc133cf96432d4e48f768c
asadkarim <asadkarim33@gmail.com>
2020-08-30 04:59:15 +0500

    still same work is left in cards

--- assets/components/mcards.js
+++ assets/components/mcards.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { ProgressBar, Colors } from 'react-native-paper';
 import {Icon} from '@material-ui/core/Icon';
 import { MaterialIcons } from '@expo/vector-icons'; 
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
@@ -17,7 +18,9 @@ export default function Mcards() {
        <View style={styles.card} >
            <View>
            <Text>title of the recording <MaterialIcons style={styles.icons} name="delete" size={24} color="gray" /> <Entypo name="share" size={24} color="grey" /> <Entypo name="heart" size={24} color="red" /> </Text>
-           <Text>24/08/2020</Text> 
+           <Text>24/08/2020</Text>
+           <Text style={styles.round}><Entypo name="controller-play" size={34} color="green" /></Text>  
+          <Text><ProgressBar progress={0.5} color={Colors.black} /></Text>
            </View>
            <View style={styles.card1} >
           
@@ -27,6 +30,7 @@ export default function Mcards() {
        <View>
            <Text>title of the recording <MaterialIcons style={styles.icons} name="delete" size={24} color="gray" /> <Entypo name="share" size={24} color="grey" /> <Entypo name="heart" size={24} color="red" /></Text> 
            <Text>24/08/2020</Text>
+           <Text style={styles.round}><Entypo name="controller-play" size={34} color="green" /></Text>
            </View>
            <View style={styles.card1} >
            
@@ -36,6 +40,7 @@ export default function Mcards() {
        <View>
            <Text>title of the recording <MaterialIcons style={styles.icons} name="delete" size={24} color="gray" /> <Entypo name="share" size={24} color="grey" /> <Entypo name="heart" size={24} color="red" /></Text> 
            <Text>24/08/2020</Text>
+           <Text style={styles.round}><Entypo name="controller-play" size={34} color="green" /></Text>
            </View>
            <View style={styles.card1} >
            
@@ -45,6 +50,7 @@ export default function Mcards() {
        <View>
            <Text>title of the recording <MaterialIcons style={styles.icons} name="delete" size={24} color="gray" /> <Entypo name="share" size={24} color="grey" /> <Entypo name="heart" size={24} color="red" /></Text> 
            <Text>24/08/2020</Text>
+           <Text style={styles.round}><Entypo name="controller-play" size={34} color="green" /></Text>
            </View>
            <View style={styles.card1} >
            
@@ -54,6 +60,7 @@ export default function Mcards() {
        <View>
            <Text>title of the recording <MaterialIcons style={styles.icons} name="delete" size={24} color="gray" /> <Entypo name="share" size={24} color="grey" /> <Entypo name="heart" size={24} color="red" /></Text> 
            <Text>24/08/2020</Text>
+           <Text style={styles.round}><Entypo name="controller-play" size={34} color="green" /></Text>
            </View>
            <View style={styles.card1} >
            
@@ -84,7 +91,7 @@ const styles = StyleSheet.create({
   card:{
     flex:1,
     flexDirection:'row',
-    height:100,
+    height:110,
     width:"90%",
     backgroundColor:"#F6F6F6",
     borderRadius:9,
@@ -114,5 +121,20 @@ const styles = StyleSheet.create({
 
   icons: {
     paddingLeft:90,
+  },
+  round: {
+    flex:1,
+    flexDirection:'row',
+    borderColor: "#adff2f",
+        borderRadius: 100,
+        backgroundColor:'white',
+        width:45,
+        marginTop:10,
+        height:100,
+        textAlign:'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 1.5,
+        shadowRadius: 9, 
   }
 });
